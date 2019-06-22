0be41c3e1639ecc869fbc27dd9e62d52f8d03aa4
Cuong Duy Nguyen <ndc07.it@gmail.com>
2019-06-22 22:40:00 +0700

    Complete data flow for multi file (#7)

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
 const fs = require('fs');
 const extractDocxFile = require('./utils/extractDocxFile');
-const getTableOfContents = require('./utils/getTableOfContents');
+const getTOC = require('./utils/getTOC');
 const findIndexOfTOC = require('./utils/findIndexOfTOC');
 const writeResultFiles = require('./utils/writeResutFiles');
 
 const extractDocxFiles = async () => {
-  const path = './docx-files/Group-6-Topic_Detection_final.docx';
-  await extractDocxFile(path);
-}
+  await extractDocxFile();
+};
 
-const app = () => {
+const app = async () => {
   extractDocxFiles();
 
-  const data = fs.readFileSync('./documents/data.txt', 'utf8');
-  const indexOfTOC = findIndexOfTOC(data);
-  // New data, remove contents before TOC(table of contents).
-  const newData = data.substring(indexOfTOC);
+  const documentsDir = './documents';
+  const fileNames = fs.readdirSync(documentsDir, 'utf8');
+  fileNames.forEach(file => {
+    const data = fs.readFileSync(documentsDir + `/${file}`, 'utf8');
+    const indexOfTOC = findIndexOfTOC(data);
+    // New data, remove contents before TOC(table of contents).
+    const newData = data.substring(indexOfTOC);
 
-  const tableOfContents = getTableOfContents(newData);
-  const contents = newData.substring(tableOfContents.nextIndex);
+    const tableOfContents = getTOC(newData);
+    const contents = newData.substring(tableOfContents.nextIndex);
 
-  writeResultFiles(tableOfContents.value, contents)
+    writeResultFiles(tableOfContents.value, contents);
+  });
 };
 
 app();
--- utils/extractDocxFile.js
+++ utils/extractDocxFile.js
@@ -2,11 +2,26 @@ const fs = require('fs');
 const textract = require('textract');
 const thinid = require('thinid');
 
-module.exports = path => {
-  textract.fromFileWithPath(path, (err, text) => {
-    if (err) {
-      return err;
-    }
-    fs.writeFileSync('./documents/data.txt', text, 'utf8');
+module.exports = () => {
+  const docxDir = './docx-files';
+  if (!fs.existsSync(docxDir)) {
+    fs.mkdirSync(docxDir);
+  }
+
+  const fileNames = fs.readdirSync(docxDir, 'utf8');
+  fileNames.forEach(file => {
+    const path = `./docx-files/${file}`
+    textract.fromFileWithPath(path, (err, text) => {
+      if (err) {
+        throw new Error(err);
+      }
+
+      const dir = './documents';
+      if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir);
+      }
+
+      fs.writeFileSync(`./documents/${thinid()}.txt`, text, 'utf8');
+    });
   });
 };
