f234bc68919a1700c2e66dd2fd4f3281124e414c
Cuong Duy Nguyen <ndc07.it@gmail.com>
2019-06-22 22:39:11 +0700

    Complete data flow for multi file

--- utils/writeResutFiles.js
+++ utils/writeResutFiles.js
@@ -2,7 +2,20 @@ const fs = require('fs');
 const thinid = require('thinid');
 
 module.exports = (tableOfContents, contents) => {
+  const tocDir = './table-of-contents';
+  const contentsDir = './contents';
+  if (!fs.existsSync(tocDir)) {
+    fs.mkdirSync(tocDir);
+  }
+  if (!fs.existsSync(contentsDir)) {
+    fs.mkdirSync(contentsDir);
+  }
+
   const fileName = thinid();
-  fs.writeFileSync(`./table-of-contents/${fileName}.txt`, tableOfContents, 'utf8');
+  fs.writeFileSync(
+    `./table-of-contents/${fileName}.txt`,
+    tableOfContents,
+    'utf8'
+  );
   fs.writeFileSync(`./contents/${fileName}.txt`, contents, 'utf8');
-}
+};
