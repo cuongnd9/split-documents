feead37030d1b6a4732456f96f325c935ca49f0c
Cuong Duy Nguyen <ndc07.it@gmail.com>
2019-06-22 15:14:39 +0700

    Read docx file (#3)

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('👋 Xin Chào');
+const fs = require('fs');
+const textract = require('textract');
+
+const path = './documents/Group-6-Topic_Detection_final.docx';
+
+textract.fromFileWithPath(path, (err, text) => {
+  if (err) {
+    return err;
+  }
+  fs.writeFileSync('./data.txt', text, 'utf8');
+})
